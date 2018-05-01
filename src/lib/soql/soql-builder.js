ab79ea08dbff8ae258de1fe43f853bcfd8370ca3
allejo <allejo@me.com>
2018-05-01 01:11:23 -0700

    Allow chaining in SoqlBuilder

--- src/lib/soql/soql-builder.js
+++ src/lib/soql/soql-builder.js
@@ -49,30 +49,44 @@ export default class SoqlBuilder {
                 }.bind(this)
             );
         }
+
+        return this;
     }
 
     where(condition: string | WhereQueryBuilder) {
         this.query.where.where(condition);
+
+        return this;
     }
 
     andWhere(condition: string | WhereQueryBuilder) {
         this.query.where.andWhere(condition);
+
+        return this;
     }
 
     orWhere(condition: string | WhereQueryBuilder) {
         this.query.where.orWhere(condition);
+
+        return this;
     }
 
     having(condition: string | WhereQueryBuilder) {
         this.query.having.where(condition);
+
+        return this;
     }
 
     andHaving(condition: string | WhereQueryBuilder) {
         this.query.having.andWhere(condition);
+
+        return this;
     }
 
     orHaving(conditions: string | WhereQueryBuilder) {
         this.query.having.orWhere(conditions);
+
+        return this;
     }
 
     orderBy(column: Array<string> | string) {
@@ -81,6 +95,8 @@ export default class SoqlBuilder {
         } else if (_.isString(column)) {
             this.query.order.push(column);
         }
+
+        return this;
     }
 
     groupBy(column: Array<string> | string) {
@@ -89,13 +105,19 @@ export default class SoqlBuilder {
         } else if (_.isString(column)) {
             this.query.group.push(column);
         }
+
+        return this;
     }
 
     limit(limit: number) {
         this.query.limit = Math.max(1, Math.round(limit));
+
+        return this;
     }
 
     offset(offset: number) {
         this.query.offset = Math.max(0, Math.round(offset));
+
+        return this;
     }
 }
